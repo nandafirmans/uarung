711d74aa7e786687d1f8a388034ef2d075c61861
nanda firmansyah <nandafirmans@gmail.com>
2018-10-01 02:22:10 +0700

    init frontend

---
+++ Uarung.Web/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Uarung.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(opt => opt.Listen(IPAddress.Loopback, 5000))
+                .UseStartup<Startup>();
+        }
+    }
+}
